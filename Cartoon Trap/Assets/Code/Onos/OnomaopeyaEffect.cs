28e8369baba4683a5d39bb7ec581365a9ebc0437
marckiarck <marcos.romano@expersor.es>
2021-11-24 13:44:00 +0100

    Implementado esqueleto de las Onomatopeyas
    Implementación del esqueleto de las onomatopeyas. OnomatopeyaEffect y las llamadas a esta estan pendientes de cambios.

---
+++ Cartoon Trap/Assets/Code/Onos/OnomaopeyaEffect.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnomaopeyaEffect : MonoBehaviour
+{
+    public void ExecuteEffect()
+    {
+        print("Efect");
+        Destroy(gameObject);
+    }
+}
