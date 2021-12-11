275fac2cdfd620d9d69e0e987cdb10843f4970af
fran5xp <fran5xp@gmail.com>
2021-12-11 14:16:17 +0100

    Implementación CurrentHealth inicial.

--- Cartoon Trap/Assets/Code/UI/Game/Hud.cs
+++ Cartoon Trap/Assets/Code/UI/Game/Hud.cs
@@ -9,17 +9,60 @@ public class Hud : MonoBehaviour
     public Image StatusVida2;
     public Image StatusVida3;
 
+    public PlayerController player;
     public Health playerHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerHealth = GameObject.FindGameObjectWithTag("PlayerPrefab").GetComponent<Health>();
+        StatusVida.enabled = false;
+        StatusVida2.enabled = false;
+        StatusVida3.enabled = false;
+        //playerHealth = GameObject.FindGameObjectWithTag("PlayerPrefab").GetComponent<Health>();
+        switch (player.playerHealth.CurrentHealth)
+        {
+            case 3:
+                StatusVida3.enabled = true;
+                StatusVida2.enabled = true;
+                StatusVida.enabled = true;
+                break;
+            case 2:
+                StatusVida2.enabled = true;
+                StatusVida.enabled = true;
+                break;
+            case 1:
+                StatusVida.enabled = true;
+                break;
+            case 0:
+                break;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        StatusVida.enabled = false;
+        StatusVida2.enabled = false;
+        StatusVida3.enabled = false;
+        //playerHealth = GameObject.FindGameObjectWithTag("PlayerPrefab").GetComponent<Health>();
+        switch (player.playerHealth.CurrentHealth)
+        {
+            case 3:
+                StatusVida3.enabled = true;
+                StatusVida2.enabled = true;
+                StatusVida.enabled = true;
+                break;
+            case 2:
+                StatusVida2.enabled = true;
+                StatusVida.enabled = true;
+                break;
+            case 1:
+                StatusVida.enabled = true;
+                break;
+            case 0:
+                break;
+
+        }
     }
 }
