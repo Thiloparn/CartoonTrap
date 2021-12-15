960a29e32521e121e0eebf08399a3cd0bbc52d49
marckiarck <marcos.romano@expersor.es>
2021-12-15 11:16:10 +0100

    Solucionado que no se lance animacion al hacer punch o slash en el aire

--- Cartoon Trap/Assets/Code/Animation/PumAnimationBehaviour.cs
+++ Cartoon Trap/Assets/Code/Animation/PumAnimationBehaviour.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IdleFullColorBehaviour : StateMachineBehaviour
+public class PumAnimationBehaviour : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         PlayerController player = animator.GetComponent<PlayerController>();
-        player.playerAnimator.EndHealingAnimation(player);
-        player.playerAnimator.EndDashingAnimation(player);
-        player.playerAnimator.EndGrappingAnimation(player);
+        player.playerAnimator.EndUsingHammerAnimation(player);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
